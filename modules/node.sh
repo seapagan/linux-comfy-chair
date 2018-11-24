787c24cae5ef06763c88efeb309dc73b27de5a8a
Grant Ramsay <seapagan@gmail.com>
2018-11-24 15:53:54 +0000

    Update NVM to latest version Add bash shebang to file

--- modules/node.sh
+++ modules/node.sh
@@ -1,10 +1,11 @@
+#!/usr/bin/env bash
 # node.sh
 # Install nvm, the latest LTS version of Node and the latest actual version of Node..
 
 echo "## Setting up NVM (Node Version Manager) ##"
 echo >> ~/.bashrc
 echo "# Set up NVM" >> ~/.bashrc
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.4/install.sh | bash
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
