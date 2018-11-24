d54ec264a215eb1801e086a57939b39d4a51621f
Grant Ramsay <seapagan@gmail.com>
2018-11-24 15:58:54 +0000

    Fix the line continuation on packages.sh, slash was wrong direction. Add bash shebang to file

--- modules/packages.sh
+++ modules/packages.sh
@@ -1,11 +1,13 @@
+#!/usr/bin/env bash
 # packages.sh
 # make sure we have the required libraries and tools already installed before starting.
 
-sudo apt install -y build-essential curl gettext libssl-dev libreadline-dev /
-                    zlib1g-dev sqlite3 libsqlite3-dev libgtk2.0-0 libbz2-dev /
+# now install some important libraries for the next stages...
+sudo apt install -y build-essential curl gettext libssl-dev libreadline-dev \
+                    zlib1g-dev sqlite3 libsqlite3-dev libgtk2.0-0 libbz2-dev \
                     libxml2-dev libdb-dev gedit ccache libffi-dev
 
-# install winbind and support lib to ping WINS hosts
+# install winbind and its support lib to ping WINS hosts
 sudo apt install -y winbind libnss-winbind
 # need to append to the /etc/nsswitch.conf file to enable if not already done ...
 if ! grep -qc 'wins' /etc/nsswitch.conf ; then
