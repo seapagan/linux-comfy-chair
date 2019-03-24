dbb5e52d10738fbdd3e90ec24ed8efc7ffa59fe7
Grant Ramsay <seapagan@gmail.com>
2019-03-24 17:06:34 +0000

    Temp push for dev machine migration
    WIP for adding missing repositories on Ubuntu Server
    
    Signed-off-by: Grant Ramsay <seapagan@gmail.com>

--- modules/updates.sh
+++ modules/updates.sh
@@ -1,5 +1,11 @@
+#!/usr/bin/env bash
 # updates.sh
 # ensure we have the latest packages, including git and sublime text repos
+
+# Later versions of Ubuntu server dont have the full sources.list for apt which restricts us
+# a bit so we need to add them.
+# Before pushing to live we need to confirm they are actually missing...
+#sudo sed -i 's/$/ restricted universe multiverse/' /etc/apt/sources.list
 
 sudo add-apt-repository ppa:git-core/ppa -y
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
