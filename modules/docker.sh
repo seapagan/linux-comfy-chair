24b22825479a1d22b6dbf5772b8fa1adab4058ab
Seapagan <seapagan@users.noreply.github.com>
2020-06-07 14:39:54 +0100

    Update docker.sh
    Another missing '-y' on apt install

--- modules/docker.sh
+++ modules/docker.sh
@@ -3,7 +3,7 @@
 # install docker and docker-compose
 
 # docker
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 # remove the need for sudo...
 sudo usermod -aG docker $USER
 
