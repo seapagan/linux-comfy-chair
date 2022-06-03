62f13e8ed5c76ae5cdc184838b78739ef7967b10
Grant Ramsay <seapagan@gmail.com>
2022-06-03 09:27:01 +0100

    only unstall ubuntu-wsl package on Ubuntu!
    Just in case the distro is actually Debian or otherwise.

--- modules/wsl.sh
+++ modules/wsl.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-# specific handling if we are in WSL instead of a standard linux system...
+# specific handling if we are in Ubuntu WSL instead of a standard linux
+# system ...we install a meta-package for Ubuntu after first checking we are
+# definately using Ubuntu.
 
-# install a meta-package for Ubuntu in WSL
-sudo apt install -y ubuntu-wsl
+# ...
+distro=`cat /etc/*-release | grep DISTRIB_ID | awk -F= '{print $2}'`
+if [ "$distro" == "Ubuntu" ]; then
+  sudo apt install -y ubuntu-wsl
+fi
