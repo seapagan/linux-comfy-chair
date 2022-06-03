bc117587d39b58c32be0cb24b005ac9aa1e76201
Grant Ramsay <seapagan@gmail.com>
2022-06-03 09:07:37 +0100

    update Perl version

--- modules/perl.sh
+++ modules/perl.sh
@@ -13,15 +13,15 @@ fi
 source ~/perl5/perlbrew/etc/bashrc
 
 # install perl and select it...
-perlbrew install perl-5.35.1
-perlbrew switch perl-5.35.1
+perlbrew install perl-5.36.0
+perlbrew switch perl-5.36.0
 perlbrew install-cpanm
 # set up some cpan configuration
 (echo y; echo o conf auto_commit 1; echo o conf yaml_module YAML::XS; echo o conf use_sqlite yes; echo o conf commit) | cpan
 (echo o conf prerequisites_policy follow; echo o conf build_requires_install_policy yes) | cpan
 (echo o conf colorize_output yes; echo o conf colorize_print bold white on_black; echo o conf colorize_warn bold red on_black; echo o conf colorize_debug green on_black) | cpan
 # now install useful modules for CPAN...
-cpanm Term::ReadLine::Perl --notest # we install this separately and with no tests so it will not timeout on unattended installs. Ohterwise may timeout and crash the script.
+cpanm Term::ReadLine::Perl --notest # we install this separately and with no tests so it will not timeout on unattended installs. Ohterwise may crash the script.
 cpanm CPAN Term::ReadKey YAML YAML::XS LWP CPAN::SQLite App::cpanoutdated Log::Log4perl XML::LibXML Text::Glob
 # Upgrade any modules that need it...
 cpan-outdated -p | cpanm
