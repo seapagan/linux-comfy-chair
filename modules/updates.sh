dbb9b5a1f646720da1aa5846b59d521f093d8ca2
Grant Ramsay <seapagan@gmail.com>
2023-02-21 10:47:12 +0000

    tweak docker repository settings

--- modules/updates.sh
+++ modules/updates.sh
@@ -53,7 +53,7 @@ sudo echo \
   $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list > /dev/null
 
 # add the official Docker repo so we can install recent versions if needed...
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/$flavour/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$flavour \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
