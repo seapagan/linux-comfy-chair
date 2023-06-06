58399be3cb7496e5325cee3c52470512b3cf718a
Grant Ramsay <seapagan@gmail.com>
2023-06-06 13:41:57 +0100

    fix bug with installing z.sh

--- modules/extras.sh
+++ modules/extras.sh
@@ -3,8 +3,8 @@
 # extra additions not in the main install script
 
 # install 'z' tool (https://github.com/rupa/z)
-\curl https://raw.githubusercontent.com/rupa/z/master/z.sh ~/.z.sh
-chmod +x ~/.z.sh
+\curl https://raw.githubusercontent.com/rupa/z/master/z.sh -o ~/z.sh
+chmod +x ~/z.sh
 # set up the z tool in the shell
 if ! grep -qc 'z.sh' ~/.bashrc ; then
   echo >> ~/.bashrc
