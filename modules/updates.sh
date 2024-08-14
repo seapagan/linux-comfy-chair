7dc665c54ae855d06d402e0462038f339d20226d
Grant Ramsay <seapagan@gmail.com>
2024-08-14 16:35:50 +0100

    add the GitHub CLI

--- modules/updates.sh
+++ modules/updates.sh
@@ -58,6 +58,11 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/$flavour \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+# add the GitHub CLI repo so we can install recent versions if needed...
+wget -qO- https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null 
+sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
 
 # update then upgrade...
 echo ""
